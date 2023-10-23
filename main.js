aef989a7518f910a6318855d1c1987ccb16efe2e
Nino Ibarra <mrninoibarra@gmail.com>
2023-10-22 22:33:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Nino Ibarra - Web Developer</title>
+</head>
+<body>
+    <header>
+        <h1>Nino Ibarra</h1>
+        <p>Web Developer</p>
+    </header>
+
+    <section id="intro">
+        <h2>Intro</h2>
+        <p>Hi, I'm Nino Ibarra, I am a professor and a passionate web developer located in Sterling Heights, Michigan.</p>
+    </section>
+
+    <section id="about">
+        <h2>About Me</h2>
+        <p>I'm a business professor and web developer, merging academic knowledge
+           with real-world expertise to empower students and businesses alike in navigating the digital landscape.</p>
+        <p>Outside of teaching and web development, I enjoy biking, hiking, and camping.</p>
+    </section>
+
+    <section id="contact">
+        <h2>Contact</h2>
+        <ul>
+        <li><a href="https://www.linkedin.com/in/nino-ibarra-aa910b68">LinkedIn Profile</a></li>
+            <li><a href="https://github.com/onixibarra">GitHub Profile</a></li>
+            <li>Email: <a href="mailto:mrninoibarra@gmail.com"></a></li>
+        </ul>
+    </section>
+</body>
+</html>
